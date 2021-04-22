449de51bdfe8cdac82cccb5b979a134d6a4a2ae4
Gordilja <milan.gordic3@hotmail.com>
2021-04-22 19:31:14 +0200

    Adjust death animacija//

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -8,8 +8,35 @@ public class CameraControl : MonoBehaviour
 
     void Update()
     {
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("Zombie");
         target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
         rotation = Quaternion.Euler(target.y, -target.x, 0);
         player.transform.rotation = rotation;
     }
+
+    /*
+    public GameObject FindClosestEnemy(float min, float max)
+    {
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("Zombie");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+
+        // calculate squared distances
+        min = min * min;
+        max = max * max;
+
+        foreach (GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance && curDistance >= min && curDistance <= max)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+    */
 }
